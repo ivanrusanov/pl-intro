215c95eecd4060faa3ad877e8448ef2449c1b129
Ivan Rusanov <iWX1182659@china.huawei.com>
2022-07-15 17:54:52 +0300

    Task from lecture

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿int[] array = new int[20];
+fillArrayWithRandomNumbers(array);
+printArray(array);
+Console.WriteLine("Max element is " + findMax(array));
+
+
+void fillArrayWithRandomNumbers(int[] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        array[i] = new Random().Next(0, 100);
+        i++;
+    }
+}
+
+void printArray(int[] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.Write(array[i] + " ");
+        i++;
+    }
+    Console.WriteLine();
+}
+
+int findMax(int[] array)
+{
+    int i = 1;
+    int max = array[0];
+    while (i < array.Length)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        i++;
+    }
+    return max;
+}
